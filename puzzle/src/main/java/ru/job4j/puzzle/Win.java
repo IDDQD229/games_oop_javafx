30aba774cd9ae74a1de1a7fd791c62bde91ab493
IDDQD <bigsobolev@yandex.ru>
2021-09-16 13:13:28 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,28 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
+        boolean rsl = false;
+        for (int index = 0; index < board.length; index++) {
+            if (board[index][index] == 1) {
+                int countX = 0;
+                int countY = 0;
+                for (int j = 0; j < board.length; j++) {
+                    if (board[index][j] == 1) {
+                        countX++;
+                    }
+                    if (board[j][index] == 1) {
+                        countY++;
+                    }
+                    if (countY == board.length || countX == board.length) {
+                        rsl = true;
+                        break;
+                    }
+
+                }
+
+            }
+
+        }
         return rsl;
     }
 }
