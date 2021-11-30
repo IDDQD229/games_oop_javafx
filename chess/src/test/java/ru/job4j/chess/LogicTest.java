2c09bec8afc7f5522a39f7eccd1bfe16cd6394f4
IDDQD <bigsobolev@yandex.ru>
2021-11-30 17:07:42 +0300

    2. Каркас шахматной доски [#792]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,18 +1,36 @@
 package ru.job4j.chess;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
 
+
 public class LogicTest {
 
-    @Ignore
-    @Test
-    public void move()
+    @Test(expected = FigureNotFoundException.class)
+    public void WhenFigureNotFound()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
-        logic.add(new BishopBlack(Cell.C1));
         logic.move(Cell.C1, Cell.H6);
+    }
+
+    @Test(expected = OccupiedCellException.class)
+    public void WhenOccupiedCell()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        Cell initCell = Cell.C1;
+        logic.add(new BishopBlack(initCell));
+        Cell destCell = Cell.H6;
+        logic.add(new BishopBlack(destCell));
+        logic.move(initCell, destCell);
+    }
+
+    @Test(expected = ImpossibleMoveException.class)
+    public void WhenImpossibleMove()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        Cell initCell = Cell.C1;
+        logic.add(new BishopBlack(initCell));
+        logic.move(initCell, Cell.C3);
     }
 }
---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.chess.firuges.black;
+
+import org.junit.Test;
+import ru.job4j.chess.Logic;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+import static org.junit.Assert.*;
+
+
+public class BishopBlackTest {
+
+    @Test
+    public void whenPositionTrue() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        Cell expected = bishopBlack.position();
+        Cell actual = Cell.C8;
+        assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void whenCopyTrue() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.F8);
+        Figure actual = bishopBlack.copy(Cell.C5);
+        Cell actualPos = actual.position();
+        assertEquals(Cell.C5, actualPos);
+    }
+
+    @Test
+    public void way(){
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Cell[] actual = bishopBlack.way(Cell.G5);
+        Cell[] expected = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        assertArrayEquals(expected,actual);
+    }
+
+}
