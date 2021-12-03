7922bee30e210b2359b35c73d45072c41a3d07b4
IDDQD <bigsobolev@yandex.ru>
2021-12-03 16:34:46 +0300

    2. Каркас шахматной доски [#792]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -23,7 +23,7 @@ public final class Logic {
     private void free(Cell[] steps) throws OccupiedCellException {
         for (Figure figure : figures) {
             for (Cell step : steps) {
-                if (figure.position() == step) {
+                if (figure.position().equals(step)) {
                     throw new OccupiedCellException();
                 }
             }
